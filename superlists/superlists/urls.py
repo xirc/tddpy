346c9fbefb4fb9e13a4d49d957e28e222fdde8e8
xirc <xirckey@gmail.com>
2015-01-05 00:56:02 +0900

    Add unique URLs for each list
    Each list should have its own URL.

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
 
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
-        name='view_list'),
+    url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 )
