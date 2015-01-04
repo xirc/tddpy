57c8009a774fd6c74533c66ca438501294b0c027
xirc <xirckey@gmail.com>
2015-01-04 18:25:40 +0900

    Only saves item when necessary
    Home page only saves item when necessary.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -18,12 +18,21 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
