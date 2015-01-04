a2a39c480199cd07ebfda64d7c440f95a37036eb
xirc <xirckey@gmail.com>
2015-01-04 18:39:25 +0900

    Display multiple items in the table

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -54,7 +54,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a uniqueue URL for her
