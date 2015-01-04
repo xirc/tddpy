7b43885a1d52bd1fdfed387f14148a525adb96e5
xirc <xirckey@gmail.com>
2015-01-04 16:25:59 +0900

    Dummy view
    First unit test and url mapping, dummy view.

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'lists.views.home_page', name='home'),
 )
