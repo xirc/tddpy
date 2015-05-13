e1114e2af2c4a5610020d901145fce3f2ddd56d6
xirc <xirckey@gmail.com>
2015-05-13 21:11:36 +0900

    Write models layer test

--- source/lists/tests/test_models.py
+++ source/lists/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class ItemModelTest(TestCase):
@@ -26,6 +28,13 @@ class ListModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+    def test_create_new_creates_list_and_first_item(self):
+        List.create_new(first_item_text='new item text')
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'new item text')
+        new_list = List.objects.first()
+        self.assertEqual(new_item.list, new_list)
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -53,3 +62,15 @@ class ListModelTest(TestCase):
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+    def test_create_new_optionally_saves_owner(self):
+        user = User.objects.create()
+        List.create_new(first_item_text='new item text', owner=user)
+        new_list = List.objects.first()
+        self.assertEqual(new_list.owner, user)
+
+    def test_lists_can_have_owners(self):
+        List(owner=User()) # should not raise
+
+    def test_list_owner_is_optional(self):
+        List().full_clean() # should not raise
