ba60cefc164829f166f25072f9873835fcb6c717
xirc <xirckey@gmail.com>
2015-05-24 16:26:57 +0900

    Remove run test script. makefile contains it.

--- ci/run_pytests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script run on virutalenv builder @python3
-# Run at project root
-
-cd source
-pip install -r requirements.txt
-pip install -r test_requirements.txt
-python manage.py test lists accounts
-python manage.py test functional_tests
