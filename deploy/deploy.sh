7bae935fad1604a62440e2e212131c0e6426e525
xirc <xirckey@gmail.com>
2015-05-24 20:01:59 +0900

    Rename fabfile.py to deploy.py

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source .env
-fab deploy --host=${DEPLOY_USER}@${DEPLOY_HOST} --set=app=tddpy,app_port=80
+fab -f deploy.py deploy --host=${DEPLOY_USER}@${DEPLOY_HOST} --set=app=tddpy,app_port=80
